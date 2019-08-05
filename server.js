a0a2edfcc7458e947b9fc5b0f24c6c76c1210891
sasha <propokerstar90@gmail.com>
2019-08-05 14:58:41 +0300

    commit

--- server.js
+++ server.js
@@ -1,34 +1,52 @@
-const { ApolloServer, gql } = require('apollo-server');
+const { ApolloServer, AuthenticationError } = require("apollo-server");
+const mongoose = require("mongoose");
+const fs = require("fs");
+const path = require("path");
+const jwt = require("jsonwebtoken");
 
-const todos = [
-    { task: 'Wash car', completed: false },
-    { task: 'Buy book', completed: false }
-];
+// Import typeDefs and resolvers
+const filePath = path.join(__dirname, "typeDefs.gql");
+const typeDefs = fs.readFileSync(filePath, "utf-8");
+const resolvers = require("./resolvers");
 
-const typeDefs = gql`
-    type Todo {
-        task: String
-        completed: Boolean      
-    }
-    
-    type Query {
-        getTodos: [Todo]
-    }
-`;
+// Import Environment Variables and Mongoose Models
+require("dotenv").config({ path: "variables.env" });
+const User = require("./models/User");
+const Post = require("./models/Post");
 
-const resolvers = {
-    Query: {
-        getTodos: function () {
-            return todos;
-        }
+// Connect to MLab Database
+mongoose
+  .connect('mongodb://localhost:27017/vue-share', { useNewUrlParser: true })
+  .then(() => console.log("DB connected"))
+  .catch(err => console.error(err));
+
+// Verify JWT Token passed from client
+const getUser = async token => {
+  if (token) {
+    try {
+      return await jwt.verify(token, process.env.SECRET);
+    } catch (err) {
+      throw new AuthenticationError(
+        "Your session has ended. Please sign in again."
+      );
     }
+  }
 };
 
+// Create Apollo/GraphQL Server using typeDefs, resolvers, and context object
 const server = new ApolloServer({
-    typeDefs,
-    resolvers
+  typeDefs,
+  resolvers,
+  formatError: error => ({
+    name: error.name,
+    message: error.message.replace("Context creation failed:", "")
+  }),
+  context: async ({ req }) => {
+    const token = req.headers["authorization"];
+    return { User, Post, currentUser: await getUser(token) };
+  }
 });
 
-server.listen().then(({url}) => {
-    console.log(`Server listening on localhost ${url}`)
+server.listen().then(({ url }) => {
+  console.log(`Server listening on ${url}`);
 });
